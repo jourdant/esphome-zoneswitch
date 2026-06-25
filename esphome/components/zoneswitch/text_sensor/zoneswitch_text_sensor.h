27eb785d1fb392848078934da30de1011e5c0605
Jourdan Templeton <j@en.vc>
2026-06-25 22:16:36 +1000

    Added node address back

---
+++ esphome/components/zoneswitch/text_sensor/zoneswitch_text_sensor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/core/component.h"
+#include "../zoneswitch.h"
+
+namespace esphome {
+namespace zoneswitch {
+
+enum TextSensorMetric {
+  TEXT_SENSOR_METRIC_NODE_ADDRESS = 0,
+};
+
+class ZoneSwitch;
+
+class ZoneSwitchTextSensor : public text_sensor::TextSensor, public Component, public ZoneSwitchDiagnosticListener {
+ public:
+  void set_parent(ZoneSwitch *parent) { this->parent_ = parent; }
+  void set_metric(TextSensorMetric metric) { this->metric_ = metric; }
+
+  void on_diagnostics_update(uint8_t node_addr, bool online, uint32_t rx_ok_count, uint32_t rx_bad_count) override;
+  void dump_config() override;
+
+ protected:
+  ZoneSwitch *parent_{nullptr};
+  TextSensorMetric metric_{TEXT_SENSOR_METRIC_NODE_ADDRESS};
+};
+
+}  // namespace zoneswitch
+}  // namespace esphome
